298490727ba5249e32d1fa9ced2a6d4ebac5bf8d
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-08-05 15:04:10 +0200

    yy

--- frontend/js/inscription.js
+++ frontend/js/inscription.js
@@ -1,3 +1,4 @@
 document.querySelector("form").addEventListener("submit",(event)=>{
     event.preventDefault();
+    let formdata = new FormData(document.querySelector("form"),document.querySelector("input[type=submit]"));
 })
