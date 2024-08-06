cb5a5b8b2500a57a552cc024e6b5d35a1b442701
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-08-06 09:38:35 +0200

    yy

--- frontend/js/accueil.js
+++ frontend/js/accueil.js
@@ -3,6 +3,7 @@ const searchParams = url.searchParams;
 const username = searchParams.get("username");const password = searchParams.get("password");
 if(username == null || username == undefined || username.trim() === "")window.location = "./index.html";
 else if(password == null || password == undefined || password.trim() === "")window.location = "./index.html";
+let idUser;
 
 fetch("http://localhost:3500/api/users/"+username+"/"+password)
 .then(response=>response.json())
@@ -10,6 +11,7 @@ fetch("http://localhost:3500/api/users/"+username+"/"+password)
     
     
     const id = data.id;
+    idUser = id;
     fetch("http://localhost:3500/api/manage/"+id)
     .then(response=>response.json())
     .then(data=>{
@@ -31,6 +33,11 @@ fetch("http://localhost:3500/api/users/"+username+"/"+password)
                 let editBtn = document.createElement("button");
                 editBtn.id = "editBtn";
                 editBtn.innerText = "Modifier";
+
+                editBtn.addEventListener("click",()=>{
+                    
+                })
+
                 taskDiv.appendChild(editBtn);
                 
                 let deleteBtn = document.createElement("button");
@@ -43,6 +50,27 @@ fetch("http://localhost:3500/api/users/"+username+"/"+password)
                 completeChk.name = "completed";
                 completeChk.id = "completed";
 
+                completeChk.addEventListener("click",(event)=>{
+                    let target = event.target;
+                    let parent = target.parentNode;
+                    let idTask = parseInt(parent.getAttribute("data-id"));
+                    console.log(parent);
+
+                    fetch("http://localhost:3500/api/manage/"+idUser+"/"+idTask+"/toggle",{
+                        method:"PUT",
+                        headers:{'Accept': 'application/json',
+                'Content-Type': 'application/json'},
+                    })
+                    .then(response=>response.json())
+                    .then(data=>{
+                        console.log(data);
+                        window.location = "./accueil.html?username="+username+"&password="+password;
+                    })
+                    .catch(error=>{
+                        console.error("error",error);
+                    })
+                })
+
 
                 fetch("http://localhost:3500/api/manage/"+id+"/"+data.id)
                 .then(response=>response.json())
@@ -54,6 +82,7 @@ fetch("http://localhost:3500/api/users/"+username+"/"+password)
                 })
                 
                 taskDiv.appendChild(completeChk);
+                taskDiv.setAttribute("data-id",data.id.toString());
 
                 tasks.appendChild(taskDiv);
             })
